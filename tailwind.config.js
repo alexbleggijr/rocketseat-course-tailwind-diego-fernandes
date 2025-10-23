9981682c37e0c4a11a0c0e6ded62b7ca5c66ba28
alexbleggijr <alexbleggijr@gmail.com>
2025-10-23 07:51:36 -0300

    criando projeto com tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    extend: {
+      colors: {
+        diego: "#333",
+      },
+    },
+  },
+  plugins: [],
+};
