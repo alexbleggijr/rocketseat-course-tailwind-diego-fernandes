afc7a6c9d559621aa6c445bc5099bf8c875a59ef
alexbleggijr <alexbleggijr@gmail.com>
2025-10-28 17:44:49 -0300

    tema dark da sidebar

--- src/components/Sidebar/Profile.tsx
+++ src/components/Sidebar/Profile.tsx
@@ -5,14 +5,16 @@ export function Profile() {
   return (
     <div className="grid grid-cols-profile items-center gap-3">
       <img
-        src="https://github.com/alexbleggijr.png"
+        src="https://github.com/diego3g.png"
         className="h-10 w-10 rounded-full"
         alt=""
       />
       <div className="flex flex-col truncate">
-        <span className="text-sm font-semibold text-zinc-700">Alex Bleggi</span>
-        <span className="truncate text-sm text-zinc-500">
-          alexbleggijr@gmail.com.br
+        <span className="text-sm font-semibold text-zinc-700 dark:text-zinc-100">
+          Diego Fernandes
+        </span>
+        <span className="truncate text-sm text-zinc-500 dark:text-zinc-400">
+          diego@rocketseat.com.br
         </span>
       </div>
       <Button type="button" variant="ghost">
